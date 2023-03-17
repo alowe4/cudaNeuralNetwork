0e5b3c5f7d15a72bdf96219e204e9643af3d604e
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-03-17 14:19:23 -0700

    sigmoid threading

--- neuralNetwork.cu
+++ neuralNetwork.cu
@@ -1,61 +1,80 @@
+// What will the neural network do?
 #include <stdio.h>
 
-__device__ double sigmoid(double x){
+__device__ float sigmoid(float x){
 	return 1.0f / (1.0f + exp(-x));
+}
+__global__ void sigmoidActivationForward(float* src, float* dst,
+                                         int Z_x_dim, int Z_y_dim) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
 
+    if (index < Z_x_dim * Z_y_dim) {
+        dst[index] = sigmoid(src[index]);
+    }
 }
 
-__device__ double dSigmoid(double x){
-	return x * (1.0f - x); 
+__device__ float dSigmoid(float x){
+	return x * (1.0f - x);
 }
 
 
-__global__ void forwardFeed(double* inputLayer, double* hiddenWeights, double* hiddenLayer, double* outputLayer, double* outputWeights, double* outputLayerBias, double* hiddenLayerBias, int numHiddenNodes, int numInputs, int numOutputs, int trainingSetIndex){
-	int i = trainingSetIndex; 
+
+
+__global__ void forwardFeedP1(float* inputLayer, float* hiddenWeights, float* hiddenLayer, float* outputLayer, float* outputWeights, float* outputLayerBias, float* hiddenLayerBias, int numHiddenNodes, int numInputs, int numOutputs, int trainingSetIndex, float* HiddenLayerAct){
+	int i = trainingSetIndex;
+	//float* arr = malloc(sizeof(float) * 4); 
 	for(int j = 0 ; j < numHiddenNodes; j++){
-		double activation = hiddenLayerBias[j];
+		HiddenLayerAct[j] =0;
+		float activation = hiddenLayerBias[j];
 		for(int k = 0; k < numInputs; k++){
 			activation += inputLayer[(i * numInputs) + k] * hiddenWeights[(k * numInputs) + j];
 		}
-		hiddenLayer[j] = sigmoid(activation);
+		HiddenLayerAct[j] = activation;
+		//printf("hiddenLayer[%d]: %f\n\n",j, hiddenLayer[j]);
+		//hiddenLayer[j] = sigmoid(activation);
 	}
+	
+	
+}
 
+__global__ void forwardFeedP2(float* inputLayer, float* hiddenWeights, float* hiddenLayer, float* outputLayer, float* outputWeights, float* outputLayerBias, float* hiddenLayerBias, int numHiddenNodes, int numInputs, int numOutputs, int trainingSetIndex, float* OutputLayerAct){
+		
 	// Compute Output Layer Activation
 	for(int j = 0; j < numOutputs; j++){
-		double activation = outputLayerBias[j];
+		OutputLayerAct[j]=0;
+		float activation = outputLayerBias[j];
 		for(int k = 0; k < numHiddenNodes; k++){
 			activation += hiddenLayer[k] * outputWeights[j + (k * numOutputs)];
 		}
-		outputLayer[j] = sigmoid(activation);
-
+		OutputLayerAct[j] = activation;
+		//outputLayer[j] = sigmoid(activation);
 	}
+
 
 
 
 }
 
-// Training Outputs Checks If Our Values Are Correct
-__global__ void backpropogate(double* trainingInputs, double* hiddenLayer, double* hiddenWeights, double* outputLayer, double* outputWeights, double* trainingOutputs, double* hiddenLayerBias, double* outputLayerBias, int numHiddenNodes, int numInputs, int numOutputs, int trainingSetIndex, double lr){
 
-	int i = trainingSetIndex; 
+__global__ void backpropogate(float* trainingInputs, float* hiddenLayer, float* hiddenWeights, float* outputLayer, float* outputWeights, float* trainingOutputs, float* hiddenLayerBias, float* outputLayerBias, int numHiddenNodes, int numInputs, int numOutputs, int trainingSetIndex, float lr){
 
-	double deltaOutput[1];
+	int i = trainingSetIndex;
+	float deltaOutput[1];
 
 	// Calcualte Mean Squared Error In Output Weights
 	for(int j = 0; j < numOutputs; j++){
-		double dError = (trainingOutputs[i * numOutputs + j] - outputLayer[j]);
+		float dError = (trainingOutputs[i * numOutputs + j] - outputLayer[j]);
 		deltaOutput[j] = dError * dSigmoid(outputLayer[j]);
 	}
 
-
-	double deltaHidden[4];
+	float deltaHidden[4];
 	// Calcuate Mean Squared Error in Hidden Weights
 	for(int j = 0; j < numHiddenNodes; j++){
-		double dError = 0.0f; 
+		float dError = 0.0f;
 		for(int k = 0; k < numOutputs; k++){
-			dError += deltaOutput[k] * outputWeights[(j * 1) + k]; 
+			dError += deltaOutput[k] * outputWeights[(j * 1) + k];
 		}
-		deltaHidden[j] = dError * dSigmoid(hiddenLayer[j]); 
+		deltaHidden[j] = dError * dSigmoid(hiddenLayer[j]);
 	}
 
 	// Apply Change in Output Weights
@@ -68,9 +87,9 @@ __global__ void backpropogate(double* trainingInputs, double* hiddenLayer, doubl
 
 	// Apply Change in Hidden Weights
 	for(int j = 0; j < numHiddenNodes; j++){
-		hiddenLayerBias[j] += deltaHidden[j] * lr; 
+		hiddenLayerBias[j] += deltaHidden[j] * lr;
 		for(int k = 0; k < numInputs; k++){
-			hiddenWeights[(k * numOutputs) + j] += trainingInputs[(i * numInputs) + k] * deltaHidden[j] * lr; 
+			hiddenWeights[(k * numOutputs) + j] += trainingInputs[(i * numInputs) + k] * deltaHidden[j] * lr;
 		}
 	}
 
